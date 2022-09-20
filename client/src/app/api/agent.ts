f39398b7c5d381c441cf6d9ceec95719293583d0
Amaury Deltenre <amaury.deltenre@missiontarget.com>
2022-09-20 15:33:12 +0200

    Login

--- client/src/app/api/agent.ts
+++ client/src/app/api/agent.ts
@@ -1,6 +1,7 @@
 import axios, { AxiosError, AxiosResponse } from "axios";
 import { toast } from "react-toastify";
 import { PaginatedResponse } from "../models/pagination";
+import { store } from "../store/configureStore";
 
 const sleep = () => new Promise(resolve => setTimeout(resolve, 500));
 
@@ -8,6 +9,12 @@ axios.defaults.baseURL = 'http://localhost:5000/api/'
 axios.defaults.withCredentials = true;
 
 const responseBody = (response: AxiosResponse) => response.data;
+
+axios.interceptors.request.use(config => {
+    const token = store.getState().account.user?.token;
+    if (token) config.headers!.Authorization = `Bearer ${token}`;
+    return config;
+})
 
 axios.interceptors.response.use(async response => {
     await sleep();
@@ -35,7 +42,7 @@ axios.interceptors.response.use(async response => {
             toast.error(data.title);
             break;
         case 401:
-            toast.error(data.title);
+            toast.error(data.title || 'Unauthorized');
             break;
         case 404:
             toast.error(data.title);
@@ -76,11 +83,18 @@ const Basket = {
     removeItem: (productId: number, quantity = 1) => requests.delete(`Basket?productId=${productId}&quantity=${quantity}`),
 }
 
+const Account = {
+    login: (values: any) => requests.post('Account/login', values),
+    register: (values: any) => requests.post('Account/register', values),
+    currentUser: () => requests.get('Account/currentUser'),
+}
+
 
 const agent = {
     Catalog,
     TestErrors,
-    Basket
+    Basket,
+    Account
 }
 
 export default agent;
--- client/src/features/basket/basketSlice.ts
+++ client/src/features/basket/basketSlice.ts
@@ -1,6 +1,7 @@
-import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+import { createAsyncThunk, createSlice, isAnyOf } from "@reduxjs/toolkit";
 import agent from "../../app/api/agent";
 import { Basket } from "../../app/models/basket";
+import { getCookie } from "../../app/util/util";
 
 interface BasketState {
     basket: Basket | null;
@@ -12,26 +13,42 @@ const initialState: BasketState = {
     status: 'idle'
 }
 
-export const addBasketItemAsync = createAsyncThunk<Basket, {productId: number, quantity?: number}>(
+export const addBasketItemAsync = createAsyncThunk<Basket, { productId: number, quantity?: number }>(
     "basket/addBasketItemAsync",
-    async ({ productId, quantity = 1}, thunkAPI) => {
+    async ({ productId, quantity = 1 }, thunkAPI) => {
         try {
             return await agent.Basket.addItem(productId, quantity);
         }
         catch (error: any) {
-            return thunkAPI.rejectWithValue({error: error.data})
+            return thunkAPI.rejectWithValue({ error: error.data })
         }
     }
 )
 
-export const removeBasketItemAsync = createAsyncThunk<void, {productId: number, quantity: number, name?: string}>(
+export const removeBasketItemAsync = createAsyncThunk<void, { productId: number, quantity: number, name?: string }>(
     "basket/removeBasketItemAsync",
-    async ({ productId, quantity}, thunkAPI) => {
+    async ({ productId, quantity }, thunkAPI) => {
         try {
             await agent.Basket.removeItem(productId, quantity);
         }
         catch (error: any) {
-            return thunkAPI.rejectWithValue({error: error.data})
+            return thunkAPI.rejectWithValue({ error: error.data })
+        }
+    }
+)
+
+export const fetchBasketAsync = createAsyncThunk<Basket>(
+    'basket/fetchBasketAsync',
+    async (_, thunkAPI) => {
+        try {
+            return await agent.Basket.get();
+        } catch (error: any) {
+            return thunkAPI.rejectWithValue({ error: error.data })
+        }
+    },
+    {
+        condition: () => {
+            if (!getCookie('buyerId')) return false;
         }
     }
 )
@@ -46,7 +63,7 @@ export const basketSlice = createSlice({
     },
     extraReducers: (builder => {
         builder.addCase(addBasketItemAsync.pending, (state, action) => {
-            state.status = 'pendingAddItem'+ action.meta.arg.productId;
+            state.status = 'pendingAddItem' + action.meta.arg.productId;
 
         });
         builder.addCase(addBasketItemAsync.fulfilled, (state, action) => {
@@ -58,8 +75,7 @@ export const basketSlice = createSlice({
             console.log(action.payload);
         });
         builder.addCase(removeBasketItemAsync.pending, (state, action) => {
-            state.status = 'pendingRemoveItem'+ action.meta.arg.productId + action.meta.arg.name;
-
+            state.status = 'pendingRemoveItem' + action.meta.arg.productId + action.meta.arg.name;
         });
         builder.addCase(removeBasketItemAsync.fulfilled, (state, action) => {
             const { productId, quantity } = action.meta.arg;
@@ -73,6 +89,14 @@ export const basketSlice = createSlice({
             state.status = 'idle';
             console.log(action.payload);
 
+        });
+        builder.addMatcher(isAnyOf(addBasketItemAsync.fulfilled, fetchBasketAsync.fulfilled), (state, action) => {
+            state.basket = action.payload;
+            state.status = 'idle';
+        });
+        builder.addMatcher(isAnyOf(addBasketItemAsync.rejected, fetchBasketAsync.rejected), (state, action) => {
+            state.status = 'idle';
+            console.log(action.payload);
         });
     })
 })
